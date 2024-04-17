a748b5ae18e480f184c8ca33e2662485b708f67d
John Doe <johndoe@example.com>
2024-04-17 19:25:00 -0400

    update build.gradle, METADATA, METADATA and app.py

--- app/src/main/python/quickfoodpricecomparison/app.py
+++ app/src/main/python/quickfoodpricecomparison/app.py
@@ -4,9 +4,12 @@ This application helps quickly convert between food prices per a unit of mass or
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
+from toga.platform import current_platform
+# from typing import Bytes
 from pint import UnitRegistry
 from pathlib import Path
 import pickle
+import time
 import os
 
 
@@ -36,7 +39,7 @@ selection_list = [{'name': 'Price per kilogram', 'mass': ureg.kilogram * 1},
                   {'name': 'Price per oz', 'mass': ureg.ounce * 1}]
 
 text_style = Pack(padding=(0, 5), font_size=14)
-button_style = Pack(padding=5, font_size=14, flex=150)
+button_style = Pack(padding=(0, 5), font_size=14, flex=150)
 
 
 def read_food_density() -> list:
@@ -59,6 +62,62 @@ def read_food_density() -> list:
 
 class main_app(toga.App):
 
+    async def import_data(self, widget) -> None:
+        """On Android, let user pick a file to restore history from."""
+        print("current_platform : ", current_platform)
+        if current_platform == "android":
+            from android.content import Intent
+
+            fileChose = Intent(Intent.ACTION_GET_CONTENT)
+            fileChose.addCategory(Intent.CATEGORY_OPENABLE)
+            fileChose.setType("*/*")
+            results = await self._impl.intent_result(Intent.createChooser(fileChose, "Choose a file"))
+            data = results['resultData'].getData()
+            context = self._impl.native
+
+            bytesJarray = bytes((context.getContentResolver().openInputStream(data).readAllBytes()))
+            data = pickle.loads(bytesJarray)  # Convert bytes to string
+            for item in data:
+                self.history_item_selection.data.append({
+                    "unit_selection": item['unit_selection'],
+                    "unit_value": item['unit_value'],
+                    "density_selection": item['density_selection'],
+                    "comment": item['comment']
+                })
+        else:
+            pass
+        return
+
+    async def export_data(self, widget) -> None:
+        epoch_time = str(int(time.time()))            
+        print("current_platform : ", current_platform)
+        if current_platform == "android":
+            from android.content import Intent
+
+            # Create an intent to open a system file picker
+            context = self._impl.native
+            default_file_name = f'quick_food_price_comparison_{epoch_time}.pickle'
+            intent = Intent(Intent.ACTION_CREATE_DOCUMENT)
+            intent.addCategory(Intent.CATEGORY_OPENABLE)
+            intent.setType('application/octet-stream')
+            intent.putExtra(Intent.EXTRA_TITLE, default_file_name)
+
+            # Start the intent and get the result
+            results = await self._impl.intent_result(intent)
+            uri = results['resultData'].getData()
+
+            # Copy the file to the selected location
+            # input_stream = context.getContentResolver().openInputStream(Uri.fromFile(File(file_path)))
+            output_stream = context.getContentResolver().openOutputStream(uri)
+            output_stream.write(self.convert_history_to_pickle())
+            output_stream.close()
+            #input_stream.close()
+            output_stream.close()
+
+            print(f'Saved {byte_count} bytes to {uri.toString()}')
+        else:
+            pass
+
     def define_local_data(self) -> None:
         """Retrieve the platforms local data path and create it if necessary."""
         self.location = Path(str(self.paths.data) +
@@ -68,18 +127,17 @@ class main_app(toga.App):
             # If the directory does not exist, create it
             os.makedirs(directory)
 
-    def load_history_from_pickle(self):
+    def load_history_from_pickle(self, location=None):
         """Read history data saved in a pickle file."""
-        print(self.location)
-        with open(self.location, 'rb') as handle:
+        if location == None:
+            location = self.location
+        print(location)
+        with open(location, 'rb') as handle:
             data = pickle.load(handle)
-        # Add an empty comment to migrate older data structure
-        for item in data:
-            if 'comment' not in item:
-                data['item']['comment'] = None
+
         return data
 
-    def convert_history_to_pickle(self) -> None:
+    def convert_history_to_pickle(self, location=None):
         """Take a dictionary of the history and save it to a pickle file locally."""
         pickleable_dict = []
         for row in self.history_item_selection.data:
@@ -91,10 +149,11 @@ class main_app(toga.App):
             })
         pickleable_dict = [i for n, i in enumerate(
             pickleable_dict) if i not in pickleable_dict[n + 1:]]
-        with open(self.location, 'wb+') as handle:
-            pickle.dump(pickleable_dict, handle,
-                        protocol=pickle.HIGHEST_PROTOCOL)
-        return
+        if location == None:
+            with open(self.location, 'wb+') as handle:
+                pickle.dump(pickleable_dict, handle,
+                            protocol=pickle.HIGHEST_PROTOCOL)        
+        return pickle.dumps(pickleable_dict, protocol=pickle.HIGHEST_PROTOCOL)
 
     def cleanup_conversion_history(self) -> None:
         """Get rid of duplicate records before they are written to the history table."""
@@ -120,6 +179,9 @@ class main_app(toga.App):
         self.item_cost_input.value = 1
         self.unit_selection.value = self.unit_selection.items[0]
         self.density_item_selection.value = self.density_item_selection.items[0]
+        self.clean_menu_items()
+        self.clear_button.style.visibility = 'hidden'
+    
 
     def load_history(self, *kwargs) -> None:
         """Take a value from the history table and load it into the input boxes."""
@@ -234,8 +296,55 @@ class main_app(toga.App):
         self.cup_1.text = round(price/(mass/density).to('cup').m, 5)
 
         print(type(price_per_unit))
+        self.clear_button.style.visibility = 'visible'
 
         return price_per_unit
+
+    def clean_menu_items(self) -> None:
+        """Reset items in menu to an empty text string."""
+        self.unit.text = ""
+        self.kg_1.text = ""
+        self.g_900.text = ""
+        self.g_800.text = ""
+        self.g_750.text = ""
+        self.g_700.text = ""
+        self.g_600.text = ""
+        self.g_500.text = ""
+        self.g_400.text = ""
+        self.g_300.text = ""
+        self.g_250.text = ""
+        self.g_200.text = ""
+        self.g_100.text = ""
+        self.g_1.text = ""
+        self.mg_1.text = ""
+        self.l_1.text = ""
+        self.ml_900.text = ""
+        self.ml_700.text = ""
+        self.ml_500.text = ""
+        self.ml_300.text = ""
+        self.ml_100.text = ""
+        self.lb_3.text = ""
+        self.lb_1.text = ""
+        self.oz_12.text =""
+        self.oz_8.text = ""
+        self.oz_6.text = ""
+        self.oz_4.text = ""
+        self.oz_2.text = ""
+        self.oz_1.text = ""        
+        self.gal_1.text = ""
+        self.gal_half.text = ""
+        self.quart_1.text = ""
+        self.pint_1.text = ""
+        self.ozf_1.text = ""
+        self.cup_1.text = ""        
+
+    def spawn_menu_items(self) -> None:
+        """Create new items in the menu."""
+        for object in self.object_spawn_pool:
+            setattr(self, object, toga.Label("", style=text_style))
+            unit_name = ureg(object.split('_')[0]) * object.split('_')[1]
+            setattr(self, object + '_label',
+                    toga.Label(f"Price per {unit_name}:", style=text_style))
 
     def startup(self) -> None:
         self.define_local_data()
@@ -245,11 +354,9 @@ class main_app(toga.App):
 
         self.object_spawn_pool = ["kg_1", "g_900", "g_800", "g_750", "g_700", "g_600", "g_500", "g_400", "g_300", "g_250", "g_200", "g_100", "g_1", "mg_1", "lb_3", "lb_1", "oz_12", "oz_8", "oz_6",
                              "oz_4", "oz_2", "oz_1", "l_1", "ml_900", "ml_700", "ml_500", "ml_300", "ml_100", "gal_1", "gal_half", "quart_1", "pint_1", "ozf_1", "cup_1"]
-        for object in self.object_spawn_pool:
-            setattr(self, object, toga.Label("", style=text_style))
-            unit_name = ureg(object.split('_')[0]) * object.split('_')[1]
-            setattr(self, object + '_label',
-                    toga.Label(f"Price per {unit_name}:", style=text_style))
+
+        # create menu items
+        self.spawn_menu_items()
 
         self.unit = toga.Label("", style=Pack(
             font_family="monospace", font_style="italic"))
@@ -329,6 +436,7 @@ class main_app(toga.App):
         # main menu
         mass_icon = r"resources/mass.png"
         volume_icon = r"resources/volume.png"
+        clear_icon = r"resources/clear.png"
         # history_icon = r"resources/history.png"
         warning_icon = r"resources/warning.png"
         option_box = toga.OptionContainer(style=Pack(flex=400),
@@ -340,10 +448,11 @@ class main_app(toga.App):
                             imperial_volume, icon=volume_icon),
         ])
         item_cost_input_label = toga.Label("Item Cost: ", style=text_style)
-        self.item_cost_input = toga.NumberInput(min=0, value=1, step=0.01)
+        self.item_cost_input = toga.NumberInput(min=0, value=1, step=0.01, style=text_style)
         comment_input_label = toga.Label(
             "Conversion Comment:", style=text_style)
-        self.comment_input = toga.TextInput(style=Pack(flex=200))
+        self.comment_input = toga.TextInput(style=text_style)
+        self.comment_input.style.flex = 200
         comment_box = toga.Box(
             children=[comment_input_label, self.comment_input])
         self.unit_selection = toga.Selection(
@@ -352,6 +461,12 @@ class main_app(toga.App):
         convert_button = toga.Button("Convert",
                                      on_press=self.perform_conversion,
                                      style=button_style)
+        self.clear_button = toga.Button(icon=clear_icon,
+                                        on_press=self.reset_input,
+                                        style=button_style)
+        self.clear_button.style.width = 75        
+        self.clear_button.style.visibility = 'hidden'
+        clear_convert_box = toga.Box(children=[convert_button, self.clear_button])
         input_form = toga.Box(style=Pack(direction=ROW, flex=25), children=[
                               item_cost_input_label, self.item_cost_input, self.unit_selection])
         ppu = toga.Box(style=Pack(direction=COLUMN, alignment="center"), children=[
@@ -362,7 +477,7 @@ class main_app(toga.App):
                                                                     food_dropdown,
                                                                     comment_box,
                                                                     toga.Divider(),
-                                                                    convert_button,
+                                                                    clear_convert_box,
                                                                     toga.Divider(),
                                                                     ppu,
                                                                     toga.Divider(),
@@ -375,12 +490,25 @@ class main_app(toga.App):
         self.main_window.content = main_box
         clear_history_command = toga.Command(self.clear_history,
                                              text="Clear Conversion History",
-                                             icon=warning_icon)
-        reset_input_command = toga.Command(self.reset_input,
-                                           text="Reset Input",
-                                           icon=warning_icon)
+                                             icon=warning_icon,
+                                             group=toga.Group.EDIT)
+        clear_input_command = toga.Command(self.reset_input,
+                                           text="Clear Input",
+                                           icon=warning_icon,
+                                           group=toga.Group.EDIT)
         self.commands.add(clear_history_command)
-        self.commands.add(reset_input_command)
+        self.commands.add(clear_input_command)
+        if current_platform == "android":
+            export_data_command = toga.Command(self.export_data,
+                                           text='Export History',
+                                           icon=warning_icon,
+                                           group=toga.Group.FILE)
+            import_data_command = toga.Command(self.import_data,
+                                           text='Import History',
+                                           icon=warning_icon,
+                                           group=toga.Group.FILE)
+            self.commands.add(import_data_command)
+            self.commands.add(export_data_command)
 
 
 def main():
